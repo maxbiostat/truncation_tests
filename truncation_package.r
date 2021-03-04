54db3e70c3bf7574c3a5c2058e6ffa94b8f1d9c3
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2021-03-04 09:33:02 -0300

    new Stan-inspired log_diff_exp() implementation

--- truncation_package.r
+++ truncation_package.r
@@ -1,17 +1,26 @@
+
+
 log_sum_exp <- function(x){
   # log(sum(exp(x - max(x)))) + max(x) ## unstable
   ans <- matrixStats::logSumExp(lx = x)
   return(ans)
 }
 ##
-log_diff_exp <- function(x, y) {
-  # if(x <= y) stop("computing the log of a negative number")
-  if(y == -Inf){
-    return (x)
-  }else{    
-    return(x + log (-expm1(y-x)))
+log1m <- function(x) {
+  return(log1p(-x))
+}
+##
+log1m_exp <- function(a) {
+  if (a > -0.693147) {
+    return(log(-expm1(a))) 
+  } else {
+    return(log1m(exp(a)));
   }
 }
+##
+log_diff_exp <- function (x, y){
+  return(x + log1m_exp(y - x))
+}  
 ##
 robust_difference <- function(x, y, log = FALSE){
   sgn <- sign(x-y)
