8733a34fadaa2c372a6e11f99c703d26a3a4fe1a
Luiz Max Carvalho <luizepidemiologia@gmail.com>
2022-01-03 09:12:09 -0300

    Two initial tests; poisson fact moments and neg bin with obs error

--- tests/test_Poisson_factorial_moments.r
+++ tests/test_Poisson_factorial_moments.r
@@ -54,3 +54,66 @@ result.R
 result.C
 result.preComp
 
+####
+##$ Table
+spit_approx <- function(lambda, order, epsilon){
+  ans <- suppressWarnings(
+    compare_approximations(
+      compute_lterm = "poisson_fact_moment",
+      theta = c(lambda, order),
+      exact = order*log(lambda),
+      max_iter = 3E5,
+      eps = epsilon)
+  ) 
+  out <- tibble::tibble(
+    mu = lambda,
+    r = order,
+    target_error = ans$target_error,
+    error = ans$error,
+    success = abs(ans$error) <= ans$target_error,
+    n_iter = ans$n_evaluations,
+    method = ans$Method
+  )
+  return(out)
+}
+
+approximation.grid <- expand.grid(
+  Mu = c(0.5, 1, 10, 100),
+  R = c(2, 3, 5, 10),
+  eps = 10^(0:6)*.Machine$double.eps
+)
+
+approximation.list <- lapply(
+  1:nrow(approximation.grid),
+  function(i){
+    linha <- approximation.grid[i, ] ## linha == Portuguese for row
+    spit_approx(lambda = linha$Mu,
+                order = linha$R,
+                epsilon = linha$eps)      
+  })
+
+approximation.dt <- do.call(rbind, approximation.list)
+
+aggregate(success~method, approximation.dt, mean)
+aggregate(success~method+target_error, approximation.dt, mean)
+
+
+#### Investigating the instances in which adaptive/naive fails
+#### but the sum with a huge (3E5) number of terms does not.
+
+approximation.dt$row_id <- paste(approximation.dt$mu,
+                                  approximation.dt$r,
+                                  approximation.dt$target_error,
+                                  sep = "_")
+( fail.adapt <- subset(approximation.dt, method == "Adaptive" & !success) )
+( fail.naive <- subset(approximation.dt, method == "Naive" & !success) )
+( fail.huge <- subset(approximation.dt, method == "Fixed_3e+05" & !success) )
+
+interesting.naive <- subset(approximation.dt,
+                            row_id %in% setdiff(fail.naive$row_id, fail.huge$row_id))
+
+interesting.adaptive <- subset(approximation.dt,
+                               row_id %in% setdiff(fail.adapt$row_id, fail.huge$row_id))
+
+interesting.adaptive
+tail(interesting.adaptive)
